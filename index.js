0188b414688229897dc10c8e88326f9763c0d71f
Julien Etienne <julienetie@users.noreply.github.com>
2017-09-10 23:24:12 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
-const div = document.getElementsByTagName('div').item(0);
+
 
 const typeIs = (value, option) => {
 	const isNullorUndefined = value === null || value === undefined;
   
   // Check the value is not null or undefined.
 	if(typeof option === 'string'){
-  	return {
-    	exist: () => !isNullorUndefined,
-      empty: () => isNullorUndefined || value === '',
-    	zero: () => isNullorUndefined || value === 0
-    }[option]();
-  }
+  	  return {
+    	    exist: () => !isNullorUndefined,
+            empty: () => isNullorUndefined || value === '',
+    	    zero: () => isNullorUndefined || value === 0
+          }[option]();
+        }
 
 	const typeOfValue = typeof value;
   // If type is a number, check if it is NAN or a valid number type.
